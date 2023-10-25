9548eb64a8534aca8d0d68ac675dc53f0f827286
cocktailpeanut <cocktailpeanuts@proton.me>
2023-10-25 03:57:56 -0400

    install.json

--- pinokio.js
+++ pinokio.js
@@ -29,7 +29,7 @@ module.exports = {
       return [{
         icon: "fa-solid fa-plug",
         text: "Install",
-        href: "install.js",
+        href: "install.json",
         params: { run: true, fullscreen: true }
       }]
     }
